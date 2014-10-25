cc5a870c5d06aa9cc5e827c2252851269db20e22
Robert Rees <shudderfix@gmail.com>
2014-10-25 23:49:48 +0100

    Implements a reverse quicksort

--- chapter1.hs
+++ chapter1.hs
@@ -8,3 +8,10 @@ my_qsort (x : xs) =
 		smaller = [a | a <- xs , a <= x ]
 		larger = [b | b <- xs , b > x]
 	in my_qsort smaller ++ [x] ++ my_qsort larger
+
+my_reverse_qsort [] = []
+my_reverse_qsort (x : xs) =
+	let
+		left = [a | a <- xs , a > x ]
+		right = [b | b <- xs , b <= x]
+	in my_reverse_qsort left ++ [x] ++ my_reverse_qsort right
